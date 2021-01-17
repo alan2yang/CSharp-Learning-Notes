e04644514ef6f509b87dcd848f3b75851935f8e0
yf-web <yf_web@163.com>
2021-01-18 00:32:04 +0800

    添加了项目但是没有写内容

---
+++ 基础/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace 基础
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello world!");
+            Console.WriteLine("hello world!");
+            Console.WriteLine("hello world!");
+            Console.WriteLine("hello world!");
+            Console.WriteLine("hello world!");
+
+        }
+    }
+}
